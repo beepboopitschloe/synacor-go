4b66c235bafcb451f9430910cf3ee68d1f4aace8
Noah Muth <noahmuth@gmail.com>
2016-01-05 12:47:59 -0600

    Add method to read 16-bit integer from binary.

--- main.go
+++ main.go
@@ -3,35 +3,45 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 )
 
-func readUint16(r io.Reader, b byte) (result uint16, err error) {
+func readUint16(r io.Reader) (result uint16, err error) {
 	var lo, hi byte
+	var bytes []byte
 
-	err := binary.Read(r, binary.LittleEndian, &lo)
-	err := binary.Read(r, binary.LittleEndian, &hi)
+	err = binary.Read(r, binary.LittleEndian, &lo)
 
 	if err != nil {
-		return 0, err
+		err = binary.Read(r, binary.LittleEndian, &hi)
 	}
+
+	bytes = append(bytes, lo)
+	bytes = append(bytes, hi)
+
+	result = binary.LittleEndian.Uint16(bytes)
+
+	return result, err
 }
 
 func main() {
-	fmt.Println("Hello world")
-
 	f, err := os.Open("data/challenge.bin")
 
 	if err != nil {
 		panic(err)
 	}
 
-	for i := 0; i < 16; i++ {
-		var b byte
+	for i := 0; i < 4; i++ {
+		var num uint16
 
-		err = binary.Read(f, binary.LittleEndian, &b)
+		num, err = readUint16(f)
 
-		fmt.Println(b)
+		if err == nil {
+			fmt.Println(num)
+		} else {
+			panic(err)
+		}
 	}
 
 	defer f.Close()
