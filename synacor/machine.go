d4f0a100b998667c1b5c21bfb96f81e023c45f72
Noah Muth <noah.muth@code42.com>
2016-01-08 10:43:24 -0600

    Add stack to Machine and test it.

--- synacor/machine.go
+++ synacor/machine.go
@@ -1,6 +1,30 @@
 package synacor
 
+const INITIAL_STACK_CAPACITY = 128
+
 type Machine struct {
 	Registers [8]uint16
 	InstrPtr  uint16
+	Memory    [32768]uint16
+	stack     []uint16
+}
+
+func NewMachine() Machine {
+	m := Machine{}
+
+	m.stack = make([]uint16, 0, INITIAL_STACK_CAPACITY)
+
+	return m
+}
+
+func (m *Machine) PushStack(val uint16) {
+	m.stack = append(m.stack, val)
+}
+
+func (m *Machine) PopStack() (result uint16) {
+	result = m.stack[len(m.stack)-1]
+
+	m.stack = m.stack[0 : len(m.stack)-1]
+
+	return result
 }
---
+++ synacor/machine_test.go
@@ -0,0 +1,27 @@
+package synacor
+
+import (
+	"testing"
+)
+
+func TestPushAndPopStack(t *testing.T) {
+	m := NewMachine()
+
+	test_length := uint16(32768)
+
+	expected := make([]uint16, test_length)
+
+	for i := uint16(0); i < test_length; i++ {
+		expected[i] = i * 4
+
+		m.PushStack(expected[i])
+	}
+
+	for i := test_length - 1; i > 0; i-- {
+		result := m.PopStack()
+
+		if result != expected[i] {
+			t.Fatalf("expected[%d] should have been %s, got %s\n", i, expected[i], result)
+		}
+	}
+}
